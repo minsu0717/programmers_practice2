24c37ffa6ddf719fd33a9f7f764a3f7f633d0ee1
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2024-02-26 14:37:09 +0900

    연습

---
+++ prac15.js
@@ -0,0 +1,11 @@
+function solution(price, money, count) {
+  var answer = 0;
+  for (let i = 1; i <= count; i++) {
+    answer += price * i;
+  }
+  if (answer - money > 0) {
+    return answer - money;
+  } else {
+    return 0;
+  }
+}
