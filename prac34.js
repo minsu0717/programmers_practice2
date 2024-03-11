cb0e665ad9ec628ba9dc05a61384fb56a4e429d9
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2024-03-11 23:30:56 +0900

    연습

--- prac34.js
+++ prac34.js
@@ -1,14 +1,5 @@
-function solution(d, budget) {
-  var answer = 0;
-  let price = 0;
-  d.sort((a, b) => {
-    return a - b;
-  });
-  d.forEach((num) => {
-    price += num;
-    if (price <= budget) {
-      answer++;
-    }
-  });
+function solution(s) {
+  var answer = "";
+  answer = [...s].sort().reverse().join("");
   return answer;
 }
