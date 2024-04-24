a21899596a36c70c9316864b463f937c817f1dbb
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2024-04-24 22:47:38 +0900

    키패드 누르기 수정 완료

--- prac54.js
+++ prac54.js
@@ -1,55 +1,58 @@
 function solution(numbers, hand) {
   var answer = "";
-  const l = [1, 4, 7];
-  const r = [3, 6, 9];
-  let leftPosition = 0;
-  let rightPosition = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    console.log("왼쪽 : ", leftPosition);
-    console.log("오른쪽 : ", rightPosition);
-    console.log("--------");
-    let leftMove = 0;
-    let rightMove = 0;
+  let leftPosition = "*";
+  let rightPosition = "#";
 
-    if (l.includes(numbers[i])) {
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 1 || numbers[i] === 4 || numbers[i] === 7) {
       answer += "L";
       leftPosition = numbers[i];
-    } else if (r.includes(numbers[i])) {
+    } else if (numbers[i] === 3 || numbers[i] === 6 || numbers[i] === 9) {
       answer += "R";
       rightPosition = numbers[i];
     } else {
-      console.log(numbers[i]);
-      if (Math.abs(numbers[i] - leftPosition) === 3) {
-        leftMove = 1;
-      } else {
-        leftMove = Math.abs(numbers[i] - leftPosition);
-      }
-      if (Math.abs(numbers[i] - rightPosition) === 3) {
-        rightMove = 1;
-      } else {
-        rightMove = Math.abs(numbers[i] - rightPosition);
-      }
+      const leftDistance = getDistance(leftPosition, numbers[i]);
+      const rightDistance = getDistance(rightPosition, numbers[i]);
 
-      console.log(leftMove);
-      console.log(rightMove);
-      if (leftMove < rightMove) {
+      if (leftDistance < rightDistance) {
         answer += "L";
         leftPosition = numbers[i];
-      } else if (leftMove > rightMove) {
+      } else if (leftDistance > rightDistance) {
         answer += "R";
         rightPosition = numbers[i];
-      } else if (leftMove === rightMove) {
-        console.log(numbers[i]);
-        if (hand === "right") {
-          answer += "R";
-          rightPosition = numbers[i];
-        } else {
+      } else {
+        if (hand === "left") {
           answer += "L";
           leftPosition = numbers[i];
+        } else {
+          answer += "R";
+          rightPosition = numbers[i];
         }
       }
     }
   }
 
   return answer;
+}
+
+function getDistance(position, target) {
+  const keypad = {
+    1: [0, 0],
+    2: [0, 1],
+    3: [0, 2],
+    4: [1, 0],
+    5: [1, 1],
+    6: [1, 2],
+    7: [2, 0],
+    8: [2, 1],
+    9: [2, 2],
+    "*": [3, 0],
+    0: [3, 1],
+    "#": [3, 2],
+  };
+
+  const pos = keypad[position];
+  const tar = keypad[target];
+
+  return Math.abs(pos[0] - tar[0]) + Math.abs(pos[1] - tar[1]);
 }
